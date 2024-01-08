2bed7c0f217e6adc662004cdaf26819e2b04edb9
cirius1792 <cirolucio.tecce@gmail.com>
2024-01-08 14:45:09 +0100

    Added status bar to appearence module

--- lua/plugins/appearance/init.lua
+++ lua/plugins/appearance/init.lua
@@ -1,13 +1,20 @@
 return {
-	'catppuccin/nvim', 
-	name = 'catppuccin',
-	opts = {
-	    integrations = {
-	        treesitter = true,
-	        notify = false
-	    }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			integrations = {
+				treesitter = true,
+				notify = false,
+			},
+		},
+		config = function()
+			vim.cmd("colorscheme catppuccin")
+		end,
 	},
-	config = function()
-			vim.cmd('colorscheme catppuccin')
-		end 
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
 }
