142c4add405f06ae7f971a2d00cd2034f3a1540c
cirius1792 <cirolucio.tecce@gmail.com>
2024-01-10 19:55:44 +0100

    [QOL] Added undootree to which key mapping

--- lua/plugins/undotree/init.lua
+++ lua/plugins/undotree/init.lua
@@ -1,7 +1,11 @@
-return{
-	'mbbill/undotree',
+return {
+	"mbbill/undotree",
+	keys = {
+		{ "<leader>l", desc = "Undotree" },
+		{ "<leader>lh", desc = "Toggle Undotree" },
+	},
 	config = function()
 		-- lh = *L*ocal *H*istory
-		vim.api.nvim_set_keymap('n', '<leader>lh', ':UndotreeToggle<CR>', { noremap = true, silent = true })
-	end
+		vim.api.nvim_set_keymap("n", "<leader>lh", ":UndotreeToggle<CR>", { noremap = true, silent = true })
+	end,
 }
