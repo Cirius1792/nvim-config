c63578288f74a4fe84c547a7ee92652a2f4c0a28
cirius1792 <cirolucio.tecce@gmail.com>
2024-01-29 09:55:38 +0100

    added whichkey kelpers

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,11 +1,17 @@
 return {
- 	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      	dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<leader>f", desc = "Find" },
+		{ "<leader>ff", desc = "Find Files" },
+		{ "<leader>fg", desc = "Find Text" },
+		{ "<leader>fG", desc = "Find Git Files" },
+	},
 	config = function()
-		local builtin = require('telescope.builtin')
+		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-	end
-
+		vim.keymap.set("n", "<leader>fG", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+	end,
 }
