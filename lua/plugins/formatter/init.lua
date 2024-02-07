27737c879d1e06f38f3adfafe4b5a501b866b05c
cirius1792 <cirolucio.tecce@gmail.com>
2024-02-07 19:59:18 +0100

    moved to clang-format because of charset issues

--- lua/plugins/formatter/init.lua
+++ lua/plugins/formatter/init.lua
@@ -7,7 +7,7 @@ return {
 					require("formatter.filetypes.lua").stylua,
 				},
 				java = {
-					require("formatter.filetypes.java").google_java_format,
+					require("formatter.filetypes.java").clang_format,
 				},
 				python = {
 					require("formatter.filetypes.python").autopep8,
