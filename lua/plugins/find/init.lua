5051bd681f68df61e9e3648d4a43cf6c019d0be9
cirius1792 <cirolucio.tecce@gmail.com>
2024-02-10 11:44:49 +0100

    Fix telescope key mapping

--- lua/plugins/find/init.lua
+++ lua/plugins/find/init.lua
@@ -12,7 +12,7 @@ return {
 	config = function()
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fG", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
 	end,
 }
