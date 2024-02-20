663ae4078cd10136a962fe32c7fca20404f2c78b
Ciro Lucio Tecce <c.tecce@reply.com>
2024-02-20 18:57:09 +0100

    improved obsidian config

--- lua/plugins/obsidian/init.lua
+++ lua/plugins/obsidian/init.lua
@@ -29,5 +29,29 @@ return {
 	},
 	opts = {
 		workspaces = require("plugins.obsidian.workspaces"),
+		daily_notes = {
+			folder = "DailyNotes",
+			date_format = "%Y-%m-%d",
+			alias_format = "%B %-d, %Y",
+			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+			template = nil,
+		},
+		new_notes_location = "current_dir",
+		note_id_func = function(title)
+			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+			-- In this case a note with the title 'My new note' will be given an ID that looks
+			-- like 'my-new-note-1657296016', and therefore the file name '1657296016-my-new-note.md'
+			local suffix = ""
+			if title ~= nil then
+				-- If title is given, transform it into valid file name.
+				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			else
+				-- If title is nil, just add 4 random uppercase letters to the suffix.
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return suffix .. "-" .. tostring(os.time())
+		end,
 	},
 }
