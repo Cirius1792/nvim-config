a9e21dfb707a6190b93a193f52bcabe5b5ba1778
Ciro Lucio Tecce <c.tecce@reply.com>
2024-02-23 16:45:42 +0100

    copilot

--- lua/plugins/cmp/init.lua
+++ lua/plugins/cmp/init.lua
@@ -5,6 +5,13 @@ return {
 	config = function()
 		-- Auto Completion with CMP
 		local luasnip = require("luasnip")
+		local has_words_before = function()
+			if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+				return false
+			end
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+		end
 		local cmp = require("cmp")
 		cmp.setup({
 			snippet = {
@@ -22,8 +29,8 @@ return {
 					select = true,
 				}),
 				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
+					if cmp.visible() and has_words_before then
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
 					elseif luasnip.expand_or_jumpable() then
 						luasnip.expand_or_jump()
 					else
