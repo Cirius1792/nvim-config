8a627edc12c30606adf8c8dc6dfc8baa613a4ba1
Ciro Lucio Tecce <c.tecce@reply.it>
2024-06-05 09:26:45 +0200

    updated trouble config

--- lua/plugins/trouble/init.lua
+++ lua/plugins/trouble/init.lua
@@ -1,38 +1,37 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{ "<leader>x", desc = "+Trouble" },
-		{ "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Trouble Toggle" },
-	},
-	opts = {
-		position = "bottom", -- position of the list can be: bottom, top, left, right
-		height = 10, -- height of the trouble list when position is top or bottom
-		width = 50, -- width of the list when position is left or right
-		icons = true, -- use devicons for filenames
-		action_keys = { -- key mappings for actions in the trouble list
-			-- map to {} to remove a mapping, for example:
-			-- close = {},
-			close = "q", -- close the list
-			cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-			refresh = "r", -- manually refresh
-			jump = { "<cr>", "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
-			open_split = { "<c-x>" }, -- open buffer in new split
-			open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
-			open_tab = { "<c-t>" }, -- open buffer in new tab
-			jump_close = { "o" }, -- jump to the diagnostic and close the list
-			toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-			switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
-			toggle_preview = "P", -- toggle auto_preview
-			hover = "K", -- opens a small popup with the full multiline message
-			preview = "p", -- preview the diagnostic location
-			open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
-			close_folds = { "zM", "zm" }, -- close all folds
-			open_folds = { "zR", "zr" }, -- open all folds
-			toggle_fold = { "zA", "za" }, -- toggle fold of current file
-			previous = "k", -- previous item
-			next = "j", -- next item
-			help = "?", -- help menu
-		},
-	},
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
 }
